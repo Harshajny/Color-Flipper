7d71d2bb5dea4f496aec7a615d949ff69d0df70e
Harshajny <75127170+Harshajny@users.noreply.github.com>
2023-05-15 16:57:59 +0530

    Update script.js
    Signed-off-by: Harshajny <75127170+Harshajny@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const colors = ["rgb(0,0,0)","rgb(16,78,235)", "rgb(255,255,255)", "rgb(244,135,0)", "rgb(219,16,235)","rgb(169,14,255)", "rgb(226,235,16)"];
 const btn = document.getElementById("btn");
+const play=document.getElementById("btn");
 const color = document.querySelector(".color");
 const buttonss = document.querySelector(".btn-btn-hero");
 const navlinks = document.querySelector(".container");
 const navcenter = document.querySelector(".nav-center");
-
+btn.addEventListener('click', playMusic);
 btn.addEventListener('click', function() {
   const randomNumber = getRandomNumber(0, 5);
   console.log(randomNumber);
-
+ 
   document.body.style.backgroundColor = colors[randomNumber];
  
 navcenter.style.backgroundColor=colors[randomNumber];
@@ -21,6 +22,13 @@ navcenter.style.backgroundColor=colors[randomNumber];
 });
 
 
+
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function playMusic()
+{
+  let audio=new Audio("effect.mp3");
+  audio.play();
 }
